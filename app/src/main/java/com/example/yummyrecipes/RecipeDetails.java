5a6cdaf898260e32992bb02a293f0377b35fde18
Meriem Ikharrazen <meriem.ikharrazen@gmail.com>
2023-01-26 21:34:54 +0100

    show all instructions

--- app/src/main/java/com/example/yummyrecipes/RecipeDetails.java
+++ app/src/main/java/com/example/yummyrecipes/RecipeDetails.java
@@ -9,7 +9,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -83,6 +85,7 @@ public class RecipeDetails extends AppCompatActivity {
                 InstructionListAdapter adapter = new InstructionListAdapter(getApplicationContext(), instructions);
                 ListView listView=findViewById(R.id.instructions);
                 listView.setAdapter(adapter);
+                setListViewHeight(listView);
             }
 
             @Override
@@ -94,6 +97,27 @@ public class RecipeDetails extends AppCompatActivity {
 
 
 
+    }
+
+    public static void setListViewHeight(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null)
+            return;
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
     }
 
 }
