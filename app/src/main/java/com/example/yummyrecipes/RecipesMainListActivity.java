8cb30275e7e5686ccc06491df04a2c2baeda195c
Meriem Ikharrazen <meriem.ikharrazen@gmail.com>
2023-02-23 13:40:54 +0100

    show recipes after log in

--- app/src/main/java/com/example/yummyrecipes/RecipesMainListActivity.java
+++ app/src/main/java/com/example/yummyrecipes/RecipesMainListActivity.java
@@ -3,12 +3,66 @@ package com.example.yummyrecipes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RecipesMainListActivity extends AppCompatActivity {
+
+    private List<Recipe> recipes;
+    private ListView list;
+    private MainActivity currentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_main_list);
+
+        // Test fetching recipes
+        Retrofit mRetrofit = new Retrofit.Builder()
+                .baseUrl("https://tasty.p.rapidapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        YummlyApi test = mRetrofit.create(YummlyApi.class);
+
+        Call<Recipes> call = test.getRecipes();
+
+        call.enqueue(new Callback<Recipes>() {
+            @Override
+            public void onResponse(Call<Recipes> call, Response<Recipes> response) {
+                Log.i("FetchOrSuccess", String.valueOf(response.code()));
+                Recipes recipes = response.body();
+                Log.i("enter1", recipes.toString());
+
+                List<Recipe> recipes_list = recipes.getResults();
+                Log.i("enter2", recipes_list.toString());
+
+                for(Recipe recipe : recipes_list){
+                    Log.i("nameDebug", recipe.getName());
+                }
+
+
+                list = (ListView)findViewById(R.id.list);
+                Log.i("list", list.toString());
+
+
+                RecipeListAdapter adapter = new RecipeListAdapter(RecipesMainListActivity.this, recipes_list);
+                Log.i("MAinA", RecipesMainListActivity.this.toString());
+                list.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<Recipes> call, Throwable t) {
+                Log.e("FetchOrSuccess", t.getMessage());
+
+            }
+        });
     }
 }
